48623b64aaf60d0457fa4b620115fbd541744cfd
Lise Ostergaard <lsd@autoproff.dk>
2022-08-18 15:39:22 +0200

    new world

--- hello-world/hello-world/Program.cs
+++ hello-world/hello-world/Program.cs
@@ -7,6 +7,8 @@ namespace hello_world
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Hello new World!");
         }
     }
 }
